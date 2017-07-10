2ab1926bd8373613fb04653bdd5213c6a11d9287
rajesh <rajeshrnair95@gmail.com>
2017-07-10 11:47:08 +0530

    basic employee mel created

---
+++ server/Employee/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from Employee.models import Employee
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    """ Serializer to represent the Employee model """
+    class Meta:
+        model = Employee
+        fields = ("Emp_id", "Empfirst_name", "Emplast_name", "Emp_des", "hired_date")
--- server/urls.py
+++ server/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+#from django.conf.urls import url
+#from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+#urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+#]
+from rest_framework.routers import DefaultRouter
+from Employee.views import EmployeeViewSet
+
+router = DefaultRouter()
+router.register(prefix='employees', viewset=EmployeeViewSet)
+
+urlpatterns = router.urls
